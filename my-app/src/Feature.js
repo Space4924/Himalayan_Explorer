e4ec392473ee24245b68f07a59e729b7f89d7a3e
Space4924 <88616921+Space4924@users.noreply.github.com>
2023-10-31 09:53:08 +0530

    abcd

--- my-app/src/Feature.js
+++ my-app/src/Feature.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import Layout from './features/layout'
-import Layout2 from './features/layout2'
 import Layout1 from './features/layout1'
 
 const Feature = () => {
   return (
     <div>
-    <Layout/>
-    <Layout2/>
-    <Layout/>
     <Layout1/>
-    <Layout/></div>
+    <Layout1/>
+    <Layout1/>
+    <Layout1/>
+    <Layout1/>
+    {/* <Layout/> */}
+    </div>
   )
 }
 
